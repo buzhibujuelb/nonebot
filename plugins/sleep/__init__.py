357d4eacb73c4848eb4101ef64635d2c0c31b560
buzhibujue <1752862657@qq.com>
2020-10-06 02:23:45 +0800

    Set lowest time to zero.

--- plugins/sleep/__init__.py
+++ plugins/sleep/__init__.py
@@ -6,5 +6,5 @@ from datetime import datetime
 @on_command("sleep",privileged=True)
 async def sleep(session: CommandSession):
     await session.send("BEGIN "+datetime.now().strftime('%m/%d %H:%M:%S'))
-    await asyncio.sleep(random.randint(1,3))
+    await asyncio.sleep(random.randint(0,3))
     await session.send("END "+datetime.now().strftime('%m/%d %H:%M:%S'))
