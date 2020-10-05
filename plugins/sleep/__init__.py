c6fa7f2451c1086cee52bee29720360d51a77dc6
buzhibujue <1752862657@qq.com>
2020-10-06 02:21:32 +0800

    Add random sleep time for bot.

--- plugins/sleep/__init__.py
+++ plugins/sleep/__init__.py
@@ -1,10 +1,10 @@
 from nonebot import on_command, CommandSession
 import threading
-import time, asyncio
+import time, asyncio, random
 from datetime import datetime 
 
 @on_command("sleep",privileged=True)
 async def sleep(session: CommandSession):
     await session.send("BEGIN "+datetime.now().strftime('%m/%d %H:%M:%S'))
-    await asyncio.sleep(3)
+    await asyncio.sleep(random.randint(1,3))
     await session.send("END "+datetime.now().strftime('%m/%d %H:%M:%S'))
